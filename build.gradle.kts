5fc058ad25ffcc69c1c69a71c9be59c5c3895778
Tampwell <staleguard@tampwell.com>
2026-08-13 09:35:39 -0400

    Target IDEA 2026.2.1 (since-build 262), Kotlin 2.3.20
    2026.2 compiles to JVM target 25, which Kotlin 2.1.x cannot emit;
    2.3.20 can, and stays below the platform-bundled Kotlin 2.4.0.
    Adds pluginVerification { ides { recommended() } } for verifyPlugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,15 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea("2025.2.6.2")
+        intellijIdea("2026.2.1")
         testFramework(TestFrameworkType.Platform)
+    }
+}
+
+intellijPlatform {
+    pluginVerification {
+        ides {
+            recommended()
+        }
     }
 }
